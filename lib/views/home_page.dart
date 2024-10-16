e2b126753ff643e194db47bfd64b2288141abe0a
tanishka21 <tanishkagoel7728@gmail.com>
2024-10-16 23:10:55 +0530

    Api integration code

---
+++ lib/views/home_page.dart
@@ -0,0 +1,100 @@
+import 'package:api_integration/services/remote_services.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../model/post_model.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Post>? posts;
+  var isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    //fetch data from api
+    getData();
+  }
+
+  getData() async {
+    posts = await RemoteServices().getPosts();
+    if (posts != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          'Post',
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Visibility(
+        visible: isLoaded,
+        replacement: const Center(
+          child: CircularProgressIndicator(),
+        ),
+        child: ListView.builder(
+            itemCount: posts?.length,
+            itemBuilder: (context, index) {
+              return Container(
+                child: Row(children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Text(
+                          posts![index].title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          posts![index].body,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
+              );
+            }),
+      ),
+    );
+  }
+}
